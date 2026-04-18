a73a323b60ee675b9fd85806f2e4096d1d0b55db
Neeli Mahi Teja <mahineeli899@gmail.com>
2026-04-18 13:00:15 +0530

    feat: add EntryLoader component for loading animation

---
+++ app/components/EntryLoader.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const EntryLoader = () => {
+  return (
+    <div className="flex items-center justify-center h-40">
+      <div className="w-8 h-8 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
+    </div>
+  );
+};
+
+export default EntryLoader;
