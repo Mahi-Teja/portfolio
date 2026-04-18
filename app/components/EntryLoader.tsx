8b62ce8c873a089f33027692646a8e698cf5b2a2
Neeli Mahi Teja <mahineeli899@gmail.com>
2026-04-18 13:05:38 +0530

    feat: update EntryLoader component to accept onComplete prop

--- app/components/EntryLoader.tsx
+++ app/components/EntryLoader.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-const EntryLoader = () => {
+const EntryLoader = ({ onComplete }: { onComplete: () => void }) => {
   return (
     <div className="flex items-center justify-center h-40">
       <div className="w-8 h-8 border-4 border-gray-300 border-t-black rounded-full animate-spin"></div>
